540174a001aafa416dbaa8b33fdbd6fa5a3aee25
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-03-23 03:50:42 +0530

    Create Logger.py

---
+++ Pronova/Utils/Logger.py
@@ -0,0 +1,33 @@
+import logging
+import sys
+import os
+from logging.handlers import RotatingFileHandler
+
+LOG_FILE = "logs/pronova.log"
+
+os.makedirs("logs", exist_ok=True)
+
+LOG_FORMAT = "[%(asctime)s] [%(levelname)s] - %(name)s - %(message)s"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=LOG_FORMAT,
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+        RotatingFileHandler(LOG_FILE, maxBytes=50_000_000, backupCount=10)
+    ]
+)
+
+# Reduce spam logs
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pytgcalls").setLevel(logging.WARNING)
+logging.getLogger("ntgcalls").setLevel(logging.WARNING)
+
+LOGGER = logging.getLogger("Pronova")
+
+#
+def set_debug(enabled: bool):
+    if enabled:
+        LOGGER.setLevel(logging.DEBUG)
+    else:
+        LOGGER.setLevel(logging.INFO)
