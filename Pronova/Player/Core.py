576deda758b96466371a4d7414ec6fcedfe065a0
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-03-22 19:58:27 +0530

    Update Core.py

--- Pronova/Player/Core.py
+++ Pronova/Player/Core.py
@@ -1,17 +1,26 @@
 from pytgcalls import PyTgCalls
 
+from Pronova.Player.Handling import setup_handlers
+
 
 class EngineCore:
     def __init__(self, app):
         self.app = app
         self.core = PyTgCalls(app)
 
-        # hooks
+        self.plugin = None
         self.on_end = None
         self.on_vc_closed = None
 
-        # plugin (optional)
-        self.plugin = None
-
     async def start(self):
         await self.core.start()
+
+        setup_handlers(
+            self.core,
+            plugin=self.plugin,
+            on_end=self.on_end,
+            on_vc_closed=self.on_vc_closed
+        )
+
+    async def stop(self):
+        await self.core.stop()
