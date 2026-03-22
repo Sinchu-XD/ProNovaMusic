317bfaa99d426e9ca7d9b049fb4dceecfa6b2cb8
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-03-22 05:34:44 +0530

    Create __init__.py

---
+++ Pronova/Player/__init__.py
@@ -0,0 +1,23 @@
+from .Core import EngineCore
+from .Play import play
+from .Seek import seek
+from .Controls import (
+    stop,
+    pause,
+    resume,
+    mute,
+    unmute
+)
+from .handling import setup_handlers
+
+__all__ = [
+    "EngineCore",
+    "play",
+    "seek",
+    "stop",
+    "pause",
+    "resume",
+    "mute",
+    "unmute",
+    "setup_handlers"
+]
