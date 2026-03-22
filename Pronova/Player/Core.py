a4713a4bef53391b64a794f41a7463599f57cab7
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-03-23 02:47:15 +0530

    Update Core.py

--- Pronova/Player/Core.py
+++ Pronova/Player/Core.py
@@ -1,26 +1,11 @@
-from pytgcalls import PyTgCalls
+from .Native import _NativeEngine
+from .Controller import VoiceController
 
-from Pronova.Player.Handling import setup_handlers
 
-
-class EngineCore:
-    def __init__(self, app):
-        self.app = app
-        self.core = PyTgCalls(app)
-
-        self.plugin = None
-        self.on_end = None
-        self.on_vc_closed = None
+class VoiceEngine:
+    def __init__(self, app, cookies: str | None = None):
+        self._engine = _NativeEngine(app)
+        self.vc = VoiceController(self._engine, cookies=cookies)
 
     async def start(self):
-        await self.core.start()
-
-        setup_handlers(
-            self.core,
-            plugin=self.plugin,
-            on_end=self.on_end,
-            on_vc_closed=self.on_vc_closed
-        )
-
-    async def stop(self):
-        await self.core.stop()
+        await self._engine.start()
