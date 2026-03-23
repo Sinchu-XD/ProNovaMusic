3472c167b3f38dce53309032390c16118f465e64
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-03-24 04:58:57 +0530

    Update __init__.py

--- Pronova/Plugins/__init__.py
+++ Pronova/Plugins/__init__.py
@@ -1,2 +1,4 @@
 from .Plays import play
-from .Admins import Cont, CallBacks
+from .Admins import Cont, CallBacks, Auth
+from .Extra import BioCheck, Start, Cricket
+from .Sudo import Bans, Broadcast, Sudo, Stats
