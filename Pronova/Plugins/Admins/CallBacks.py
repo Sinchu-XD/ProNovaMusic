a526835a17281c5ac46213bca756cbf9703453d5
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-03-23 13:22:20 +0530

    Update CallBacks.py

--- Pronova/Plugins/Admins/CallBacks.py
+++ Pronova/Plugins/Admins/CallBacks.py
@@ -1,10 +1,9 @@
 from pyrogram import filters
-from pyrogram.enums import ChatMemberStatus
 import time
 
 from Pronova.Bot import bot, engine
 from Pronova.Utils.Font import sc
-from Pronova.Database import is_banned, is_gbanned
+from Pronova.Utils.Allow import admin_only
 
 
 LAST_ACTION = {}
@@ -28,26 +27,16 @@ async def safe_action(action, chat_id):
         return None
 
 
-async def is_admin(chat_id, user_id):
-    try:
-        member = await bot.get_chat_member(chat_id, user_id)
-        return member.status in (
-            ChatMemberStatus.ADMINISTRATOR,
-            ChatMemberStatus.OWNER
-        )
-    except:
-        return False
-
-
 async def safe_reply(m, text):
     if can_send(m.chat.id):
-        await m.reply(text, disable_notification=True)
+        try:
+            await m.reply(text, disable_notification=True)
+        except:
+            pass
 
 
 @bot.on_callback_query(filters.regex("^vc_"))
 async def vc_buttons(_, cq):
-
-    await cq.answer()
 
     if not cq.message:
         return
@@ -59,16 +48,8 @@ async def vc_buttons(_, cq):
     if not user or user.is_bot:
         return
 
-    uid = user.id
-
-    if await is_gbanned(uid):
-        return await cq.answer(sc("you are gbanned"), show_alert=True)
-
-    if await is_banned(chat_id, uid):
-        return await cq.answer(sc("you are banned in this chat"), show_alert=True)
-
-    if not await is_admin(chat_id, uid):
-        return await cq.answer(sc("only admins"), show_alert=True)
+    if not await admin_only(bot, cq=cq):
+        return
 
     if cq.data == "vc_skip":
         await safe_action(engine.vc.skip, chat_id)
